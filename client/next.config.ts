349541a27c7c64d891b3bec223a418a694a7387f
SorayuthJapanya <sorayuthjaapanya@gmail.com>
2026-02-03 11:20:15 +0700

    Edit: edit remorePatterns in next.config.js

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      new URL(
-        "https://cdn.dribbble.com/userupload/14898990/file/original-ba68e98ea10e1867e831884c3b153387.png?resize=1504x1128&vertical=center",
-      ),
-      new URL(
-        "https://img.freepik.com/premium-photo/memoji-emoji-handsome-smiling-man-white-background_826801-6987.jpg?semt=ais_hybrid&w=740&q=80",
-      ),
+      {
+        protocol: "https",
+        hostname: "cdn.dribbble.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
     ],
   },
 };
