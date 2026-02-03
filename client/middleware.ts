79cac679f7d124238b324992d488423d3e319fb3
SorayuthJapanya <sorayuthjaapanya@gmail.com>
2026-02-03 11:32:44 +0700

    edit: add trycatch in middleware file

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,26 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/auth/me`,
-    {
-      headers: {
-        cookie: req.headers.get("cookie") || "",
-      },
-      credentials: "include",
-    }
-  );
-
   const { pathname } = req.nextUrl;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/auth/me`,
+      {
+        headers: {
+          cookie: req.headers.get("cookie") || "",
+        },
+        credentials: "include",
+      },
+    );
 
-  if (res.status === 401 && pathname !== "/auth") {
-    return NextResponse.redirect(new URL("/auth", req.url));
+    if (res.status === 401 && pathname !== "/auth") {
+      return NextResponse.redirect(new URL("/auth", req.url));
+    }
+
+    if (res.status === 200 && pathname === "/auth") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } catch (error) {
+    console.error("Middleware Auth Check Failed:", error);
   }
-
-  if (res.status === 200 && pathname === "/auth") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
   return NextResponse.next();
 }
 
